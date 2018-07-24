9c9a90f1fd4018ec9176467429c83ea8c44bbeb6
Douglimar Moraes <douglimar@gmail.com>
2018-07-23 22:39:50 -0300

    Initial commit -- 2018.07.23 -- Publicado no Google play

---
+++ app/src/main/java/br/com/douglimar/surpresinhadiadesorte/Surpresinha.java
@@ -0,0 +1,73 @@
+package br.com.douglimar.surpresinhadiadesorte;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by Douglimar Moraes on 02/12/17.
+ *
+ *
+ */
+
+class Surpresinha {
+
+    public String generateDiaDeSorteGame() {
+
+        /* Regra do Jogo:
+         * O apostador pode escolher 7 numeros entre 31 numeros disponiveis
+         * além de 1 Mês da Sorte
+         */
+
+        Random random = new Random();
+
+        String meses[] = {"JANEIRO","FEVEREIRO", "MARÇO", "ABRIL", "MAIO", "JUNHO",
+                "JULHO", "AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO"};
+
+        String mes = meses[random.nextInt(12)];
+
+        int numsDiaDeSorte[] = new int[7];
+
+        int indice;
+        StringBuilder Retorno = new StringBuilder();
+
+        for (int i = 0; i < 7; i++) {
+            indice = random.nextInt(32);
+
+            for (int k = 0; k < 31; k++) {
+                if (consisteJogo(numsDiaDeSorte, indice) || indice == 0) {
+                    indice = random.nextInt(32);
+                }
+            }
+            numsDiaDeSorte[i] = indice;
+        }
+
+        Arrays.sort(numsDiaDeSorte);
+
+        for (int i = 0; i < 7; i++) {
+
+            if (numsDiaDeSorte[i] < 10)
+                Retorno.append(" 0").append(numsDiaDeSorte[i]);
+            else
+                Retorno.append(" ").append(numsDiaDeSorte[i]);
+        }
+
+        return Retorno.toString() + "\n\n" + "MÊS DA SORTE:\n" + mes;
+
+    }
+
+
+    private boolean consisteJogo(int pArray[], int PNumero) {
+
+        boolean Retorno = false;
+
+        for (int aPArray : pArray) {
+            if (aPArray == PNumero) {
+                Retorno = true;
+                break;
+            }
+        }
+
+        return Retorno;
+    }
+
+}
